49e2d5226f09da7381f2219269714435052743a8
Simon Lee <scmlee@gmail.com>
2019-05-12 22:30:47 -0400

    Update log file and retention

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from logzero import logging, setup_logger
 
-LOG_FILE = "trash_panda_copy.log"
+LOG_FILE = "trash_panda_cop.log"
 LOG_LEVEL = logging.INFO
 
 
@@ -8,5 +8,5 @@ def getLogger():
     ''' Return a standard configured logger '''
     return setup_logger(logfile=LOG_FILE, 
                         level=LOG_LEVEL,
-                        maxBytes=1024 * 1024, 
+                        maxBytes=1024 * 1024 * 10, 
                         backupCount=10)
